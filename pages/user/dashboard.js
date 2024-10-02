bedd8d9e851e7c4382ec9722ed05c6eb63bb4c6d
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-02 19:31:51 +0000

    updating dashboard with card

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,5 +1,5 @@
 
-import { Button, Container, Typography } from '@mui/material'
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material'
 import TemplateDefault from '../../src/templates/Default'
 
 export default function Home ()  {
@@ -15,6 +15,89 @@ export default function Home ()  {
                 <Button sx={{display: 'block', m: 'auto'}} variant='contained'>
                     Publicar novo anúncio
                 </Button>
+            </Container>
+            <Container
+                sx={{mt: 15}}
+                maxWidth="md"
+                
+            >
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6} md={4} >
+                        <Card>
+                            <CardMedia 
+                                sx={{paddingTop: '56%'}}
+                                image={'https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg'}
+                                title="Titulo do Anuncio"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    Produto X
+                                </Typography>
+                                <Typography>
+                                    R$ 60,00
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    Editar
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    Remover
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                            <CardMedia 
+                                sx={{paddingTop: '56%'}}
+                                image={'https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg'}
+                                title="Titulo do Anuncio"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    Produto X
+                                </Typography>
+                                <Typography>
+                                    R$ 60,00
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    Editar
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    Remover
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                            <CardMedia 
+                                sx={{paddingTop: '56%'}}
+                                image={'https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg'}
+                                title="Titulo do Anuncio"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    Produto X
+                                </Typography>
+                                <Typography>
+                                    R$ 60,00
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    Editar
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    Remover
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Grid>
             </Container>
         </TemplateDefault>
     )
