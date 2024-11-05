6184518e5c956da6c8c82900d5f0df4e729900c0
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-11-05 17:02:04 -0100

    feat: part 2 - integrating publish annoucement to api - using formidable to save files

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,5 @@
+import path from 'path'
+import fs from 'fs'
 
 import ProductsModel from '../models/products'
 import dbConnect from '../utils/dbConnect'
@@ -7,12 +9,42 @@ import formidable from 'formidable-serverless'
 const post = async (req ,res) => {
     await dbConnect()
 
-    const form = new formidable.IncomingForm()
+
+    const form = new formidable.IncomingForm({
+        multiples: true,
+        uploadDir: 'public/uploads',
+        keepExtensions: true,
+    })
     
     form.parse(req, (error, fields, data) => {
-        console.log('Chegou no form parser')
+        if (error) {
+            return res.status(500)
+        }
 
-        res.status(200)
+        const { files } = data
+        
+        const filesToRename = files instanceof Array 
+            ? files
+            : [files]
+
+        filesToRename.forEach((file) => {
+            const timestamp = Date.now()
+            const random = Math.floor(Math.random() * 999999999) + 1
+
+            const extension = path.extname(file.name)
+
+            const filename = `${timestamp}_${random}${extension}`
+
+            const oldpath = path.join(__dirname, `../../../../${file.path}`)
+            const newpath = path.join(__dirname, `../../../../${form.uploadDir}/${filename}`)
+
+            fs.rename(oldpath, newpath, (error) => {
+                console.log(error)
+                return res.status(500).json({success: true})
+            })
+        })
+
+        res.status(200).json({ success: true})
     })
 }
 
